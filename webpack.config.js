879fe600cde6db0702594e90c8a3faac5ea4de65
Meihui_Xiong <xiongmeihui@gwu.edu>
2018-09-03 16:35:13 -0400

    complete Output Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js', //入口文件, 就是开始打包整个项目的起点
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    }, //入口文件, 就是开始打包整个项目的起点
     output: {
-        filename: 'bundle.js', //输出文件的名字和输出位置，在这里更改
+        filename: '[name].bundle.js',
+        //输出文件的名字和输出位置，在这里更改
         path: path.resolve(__dirname, 'dist')
     }, //出口文件
     module: {
     }, //处理对应模块
-    plugins: [], //对应的插件
+    plugins: [new HtmlWebpackPlugin({
+        title: 'Output Management'
+    }),
+    new CleanWebpackPlugin('dist')], //对应的插件
     devServer: {}, //开发服务器配置
 }
