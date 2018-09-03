4342b2f7b58ef5c9d9eae07965d487ba2d9f2807
Meihui_Xiong <xiongmeihui@gwu.edu>
2018-09-03 00:22:05 -0400

    开始Output Management

--- src/index.js
+++ src/index.js
@@ -1,22 +1,11 @@
 import _ from 'lodash';
-import './style.css';
-import Myself from './myself.jpg';
-import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
 
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    //Add the image to our existing div
-    let me = new Image();
-    me.src = Myself;
-    element.appendChild(me);
-
-    //打印xml
-    console.log(Data);
     return element;
 }
 document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -7,34 +7,6 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     }, //出口文件
     module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader'
-                ]
-
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-            ]
     }, //处理对应模块
     plugins: [], //对应的插件
     devServer: {}, //开发服务器配置
