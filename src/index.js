27f79c0db6659e4e787b8ee9591ff92b61c4e2d6
Meihui_Xiong <xiongmeihui@gwu.edu>
2018-09-06 13:41:08 -0400

    完成模块热替换‘
    ’
    
    dfdsfs
    
    1111
    ```

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import _ from 'lodash';
 import printMe from './print.js'
+import './styles.css';
 
 function component() {
     var element = document.createElement('div');
@@ -8,9 +9,20 @@ function component() {
     // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+    btn.onclick = printMe; // onclick event is bind to the original printMe function
 
     element.appendChild(btn);
     return element;
 }
-document.body.appendChild(component());
+let element = component();
+document.body.appendChild(element);
+
+if(module.hot) {
+    module.hot.accept('./print.js', function () {
+        console.log('Accepting the updated printMe module!');
+
+        document.body.removeChild(element);
+        element = component(); // Re-render the "component" to update the click handler
+        document.body.appendChild(element);
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,39 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
         app: './src/index.js',
-        print: './src/print.js'
+        //print: './src/print.js'
     }, //入口文件, 就是开始打包整个项目的起点
     output: {
         filename: '[name].bundle.js',
         //输出文件的名字和输出位置，在这里更改
-        path: path.resolve(__dirname, 'dist')
+        path: path.resolve(__dirname, 'dist'),
+        //publicPath: "./xiong/"
     }, //出口文件
     module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
     }, //处理对应模块
-    plugins: [new HtmlWebpackPlugin({
-        title: 'Output Management'
-    }),
-    new CleanWebpackPlugin('dist')], //对应的插件
-    devServer: {}, //开发服务器配置
+    plugins: [
+        new HtmlWebpackPlugin({ //用来自动生成入口index.html文件
+            title: 'Output Management'
+        }),
+        new CleanWebpackPlugin('dist'),
+        new webpack.NamedModulesPlugin(),
+        new webpack.HotModuleReplacementPlugin()
+    ], //对应的插件
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+        hot: true
+        //publicPath: "/test/"
+    }, //开发服务器配置
 }
